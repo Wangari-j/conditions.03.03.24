8c35a2e2cbe607c787c2488b1fb3ef2273ac2663
Wangari-j <wangarijudy@gmail.com>
2024-03-03 16:03:14 +0300

    conditions

---
+++ conditions.py
@@ -0,0 +1,8 @@
+def less_than_or_equal_to_zero(num1):
+    result = num1 <= 0
+    return result
+
+print(less_than_or_equal_to_zero(5))
+print(less_than_or_equal_to_zero(0))
+print(less_than_or_equal_to_zero(-2))
+

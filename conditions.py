668064371f7f4109ebb215b9319d191c6dd8e39e
Wangari-j <wangarijudy@gmail.com>
2024-03-03 16:24:23 +0300

    conditions

--- conditions.py
+++ conditions.py
@@ -6,3 +6,10 @@ print(less_than_or_equal_to_zero(5))
 print(less_than_or_equal_to_zero(0))
 print(less_than_or_equal_to_zero(-2))
 
+def addition(x,y):
+    result = x + y
+    return result
+
+print(addition(3,2))
+print(addition(-3,-6))
+print(addition(7,3))
